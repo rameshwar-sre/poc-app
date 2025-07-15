4326c82a32d4173579f57ab442087c3c231e0e13
rameshwar-sre <rameshwar.dudhe@coredge.io>
2025-07-15 21:58:07 +0530

    test13

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Sample App via ArgoCD + helm!');
+  res.send('Hello from Sample App via ArgoCD + helm! test13');
 });
 
 app.listen(port, () => {
